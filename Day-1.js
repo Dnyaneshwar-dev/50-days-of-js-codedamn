e156f53ed39c06323f02672788993cb29398eb15
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2022-01-17 11:53:38 +0530

    Added day 1 solution

---
+++ Day-1.js
@@ -0,0 +1,19 @@
+function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
+  // write your solution here
+  while (true) {
+    // Math.random() return the decimal number between 0 and 1
+    // by multiplying it with rangeEnd we can insure that new number will be always <= rangeEnd
+    // If new number >= rangeStart then we will return it
+    var number = Math.random() * rangeEnd;
+
+    // converting to integer
+    number = parseInt(number);
+    // found the number between rangeStart and rangeEnd
+    if (number >= rangeStart) {
+      return number;
+    }
+  }
+  return;
+}
+
+console.log(`My random number: ${randomNumberGeneratorInRange(5, 100)}`);
