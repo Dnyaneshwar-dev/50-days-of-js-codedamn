87494c9eae30216a621e23748d4c2d91047e9ae2
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2022-01-19 09:15:18 +0530

    Added day 3 solution

--- Day-3.js
+++ Day-3.js
@@ -1,9 +1,18 @@
 const num = 3849;
 
-function reverseGivenInteger(n) {
+function reverseGivenInteger(num) {
   // write your solution here
+  var rev = 0;
 
-  return parseInt(n.toString().split("").reverse().join("")) * Math.sign(n);
+  while (num != 0) {
+    lastDigit = num % 10;
+    rev = rev * 10 + lastDigit;
+    num = Math.floor(num / 10);
+  }
+
+  return parseInt(rev);
+
+  // return parseInt(n.toString().split("").reverse().join("")) * Math.sign(n);
 }
 
 console.log(`Reversed integer is: ${reverseGivenInteger(num)}`);
