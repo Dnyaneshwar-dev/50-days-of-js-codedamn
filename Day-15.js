a200b835a6dadff013181e7ecccf1e3028dae311
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2022-02-09 19:25:25 +0530

    Added day 15 solution

---
+++ Day-15.js
@@ -0,0 +1,9 @@
+const isPangram = (input) => {
+  input = input.toUpperCase().split("");
+  for (let i = 65; i < 91; i++) {
+    let char = String.fromCharCode(i);
+    if (input.includes(char)) continue;
+    else return false;
+  }
+  return true;
+};
